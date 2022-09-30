9f3969bdd3afaf6407e22c3994b87e65e3ff0734
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-29 23:04:45 -0300

    feat: search by date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tech_news.database import search_news
 from re import compile, IGNORECASE
 
@@ -6,12 +7,20 @@ from re import compile, IGNORECASE
 def search_by_title(title):
     news_list = search_news({"title": compile(title, flags=IGNORECASE)})
     results = [(news["title"], news["url"]) for news in news_list]
-    return(results)
+    return results
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        news_list = search_news({"timestamp": formatted_date})
+        results = [(news["title"], news["url"]) for news in news_list]
+        return results
+    except ValueError:
+        raise (ValueError("Data inválida"))
 
 
 # Requisito 8
