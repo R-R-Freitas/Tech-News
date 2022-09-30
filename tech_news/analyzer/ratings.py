4dba838327aafd55f29fafd8d024a7db706f1a9e
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-30 13:41:27 -0300

    feat: top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    # solução baseada em uma das apresentadas no artigo seguinte:
+    # http://stygianvision.net/updates/python-sort-list-object-dictionary-multiple-key/
+    top_5 = sorted(
+        news_list,
+        key=lambda news: (-news["comments_count"], news["title"].lower()),
+    )[:5]
+    results = [(news["title"], news["url"]) for news in top_5]
+    return results
 
 
 # Requisito 11
